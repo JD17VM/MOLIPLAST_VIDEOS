0a1917e4bcd55cc3f85b0b07c2c2e3a95700b5f8
JD17VM <jdiegodiego4@gmail.com>
2025-01-03 18:38:40 -0500

    Se agregaron los datos de la plantilla Remotion

---
+++ backend/moliplast_video_plantilla/src/Producto_Unico/Piezas_Fondo.jsx
@@ -0,0 +1,37 @@
+
+
+const pieza_izquierda = {
+    backgroundColor: "red",
+    width:"50%",
+    height:"100%",
+
+
+        
+};
+
+
+const pieza_derecha = {
+    backgroundColor: "green",
+    width:"50%",
+    height:"100%",
+};
+
+export const Pieza_Izquierda = () => {
+    return (
+      <div style={{...pieza_izquierda}}>
+        
+      </div>
+    );
+};
+
+
+
+export const Pieza_Derecha = () => {
+    return (
+      <div style={{...pieza_derecha}}>
+        
+      </div>
+    );
+};
+  
+  
